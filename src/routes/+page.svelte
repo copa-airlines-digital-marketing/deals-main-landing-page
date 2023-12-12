09eb8ea5681a05161c4fdce753258f492933dcc6
Jose Von Chong <javch1995@gmail.com>
2023-12-12 11:45:51 -0500

    init for new top of page at promotions ALL langs

---
+++ src/routes/+page.svelte
@@ -0,0 +1,66 @@
+<div class="w-full bg-primary min-h-[256px] grid grid-rows-[64px,auto,64px] mt-0 !mb-64">
+	<div class="row-start-2">
+		<div class="flex  flex-col container mx-auto gap-24 ">	
+				<nav aria-label="Breadcrumb" class="">
+					<ol class="Breadcrumb flex gap-8 md:gap-16 items-center">
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="https://www.copaair.com/">Inicio</a>
+					</li>
+					</ol>
+				</nav>
+			<h1 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md ">¡Promociones!</h1>	
+		    <h2 class=" font-sans !text-caption md:!text-caption-large text-grey-75">Conoce nuestras más recientes promociones y tarifas especiales</h2>
+        </div>
+	</div>
+</div>
+
+
+
+<div class="w-full bg-primary min-h-[256px] grid grid-rows-[64px,auto,64px] mt-0 !mb-64">
+	<div class="row-start-2">
+		<div class="flex  flex-col container mx-auto gap-24 ">	
+				<nav aria-label="Breadcrumb" class="">
+					<ol class="Breadcrumb flex gap-8 md:gap-16 items-center">
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="https://www.copaair.com/">Home</a>
+					</li>
+					</ol>
+				</nav>
+			<h1 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md ">Deals!</h1>	
+		    <h2 class=" font-sans !text-caption md:!text-caption-large text-grey-75">Know about our exclusive fares, promotions and last minute offers</h2>
+        </div>
+	</div>
+</div>
+
+
+<div class="w-full bg-primary min-h-[256px] grid grid-rows-[64px,auto,64px] mt-0 !mb-64">
+	<div class="row-start-2">
+		<div class="flex  flex-col container mx-auto gap-24 ">	
+				<nav aria-label="Breadcrumb" class="">
+					<ol class="Breadcrumb flex gap-8 md:gap-16 items-center">
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="https://www.copaair.com/">Inicio</a>
+					</li>
+					</ol>
+				</nav>
+			<h1 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md ">Promoções</h1>	
+		    <h2 class=" font-sans !text-caption md:!text-caption-large text-grey-75">Saiba mais sobre nossas últimas promoções e tarifas especiais</h2>
+        </div>
+	</div>
+</div>
+
+<div class="w-full bg-primary min-h-[256px] grid grid-rows-[64px,auto,64px] mt-0 !mb-64">
+	<div class="row-start-2">
+		<div class="flex  flex-col container mx-auto gap-24 ">	
+				<nav aria-label="Breadcrumb" class="">
+					<ol class="Breadcrumb flex gap-8 md:gap-16 items-center">
+					<li class="text-grey-75 font-sans hover:underline active:underline">
+						<a href="https://www.copaair.com/">Deals</a>
+					</li>
+					</ol>
+				</nav>
+			<h1 class="font-heading font-heading-bold text-grey-75 text-h1-sm lg:text-h1-lg md:text-h1-md ">Promotions</h1>	
+		    <h2 class=" font-sans !text-caption md:!text-caption-large text-grey-75">Découvrez nos dernières promotions et nos tarifs spéciaux.</h2>
+        </div>
+	</div>
+</div>
